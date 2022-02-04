70b47820ddef87608fa98e11463cc5ecbdf2dc39
Guillaume CLERIS <guillaume.cleris@ensiie.fr>
2022-02-04 17:59:48 +0100

    Event binding syntax

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  onButtonClick() {
+    console.log('Button was clicked')
+  }
   title = 'pw';
 }
