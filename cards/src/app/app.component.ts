0f705f2fc74f97993752487c64662024284e22c7
Guillaume CLERIS <guillaume.cleris@ensiie.fr>
2022-02-27 20:29:34 +0100

    accepting data in a child component

--- cards/src/app/app.component.ts
+++ cards/src/app/app.component.ts
@@ -6,5 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'cards';
+  posts = [
+    { 
+      title: 'Near tree',
+      imageUrl: 'assets/tree.jpeg',
+      username: 'nature',
+      content: 'What a beautiful tree'
+    },
+    { 
+      title: 'Snowy Mountain',
+      imageUrl: 'assets/mountain.jpeg',
+      username: 'mountainlover',
+      content: 'What a beautiful Snowy Mountain'
+    },
+    { 
+      title: 'Montain biking',
+      imageUrl: 'assets/biking.jpeg',
+      username: 'biking1',
+      content: 'Do some biking'
+    }
+  ];
 }
