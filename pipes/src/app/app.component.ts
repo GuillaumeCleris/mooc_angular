461df2215d058caf1dd00c4c31c5ec0c8dda3235
Guillaume CLERIS <guillaume.cleris@ensiie.fr>
2022-03-13 18:14:06 +0100

    pipes project

--- pipes/src/app/app.component.ts
+++ pipes/src/app/app.component.ts
@@ -6,9 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  name : string;
-  
+  name : string ='';
+  date : string ='';
+  amount : number = 0;
+  height : number = 0;
+
   OnChangeName(value:string){
     this.name = value;
+  }
+  
+  OnChangeDate(value:string){
+    this.date = value;
+  }
+  
+  OnChangeAmount(value:string){
+    this.amount = parseFloat(value);
+  }
+
+  OnChangeHeight(value:string){
+    this.height = parseFloat(value);
   }
 }
