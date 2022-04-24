f5ecb1785ef32c7310204bac7e964eedcf84d1a2
Guillaume CLERIS <97674349+GuillaumeCleris@users.noreply.github.com>
2022-04-24 17:51:18 +0200

    Revert "Revert "placeholder""
    This reverts commit 43c528c3d2f7974d9de2ff0a7e8ded1ffc2bf145.

--- comps/src/app/app.module.ts
+++ comps/src/app/app.module.ts
@@ -3,8 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ElementsModule } from './elements/elements.module';
-import { CollectionsModule } from './collections/collections.module';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { HomeComponent } from './home/home.component';
 
@@ -16,8 +14,6 @@ import { HomeComponent } from './home/home.component';
   ],
   imports: [
     BrowserModule,
-    ElementsModule,
-    CollectionsModule,
     AppRoutingModule
   ],
   providers: [],
