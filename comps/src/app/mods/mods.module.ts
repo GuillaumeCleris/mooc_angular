f109cf7aa4a257ae7c60f733fe4c6e2eab36ee87
Guillaume CLERIS <97674349+GuillaumeCleris@users.noreply.github.com>
2022-04-28 19:02:50 +0200

    setup modal

--- comps/src/app/mods/mods.module.ts
+++ comps/src/app/mods/mods.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ModsRoutingModule } from './mods-routing.module';
-
+import { ModalComponent } from './modal/modal.component';
+import { ModsHomeComponent } from './mods-home/mods-home.component';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ModalComponent,
+    ModsHomeComponent
+  ],
   imports: [
     CommonModule,
-    ModsRoutingModule
+    ModsRoutingModule,
+    SharedModule
   ]
 })
 export class ModsModule { }
