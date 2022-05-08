1acb2f689cca9cfa47b3a6709345922d15a47911
Guillaume CLERIS <97674349+GuillaumeCleris@users.noreply.github.com>
2022-05-08 02:18:02 +0200

    Revert "Création du site de tennis"
    This reverts commit ce2ba28a0b2adde7e6e71023f41fafb0a16eccfc.

--- wsearch/src/app/search-bar/search-bar.component.ts
+++ wsearch/src/app/search-bar/search-bar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-search-bar',
@@ -6,7 +6,6 @@ import { Component, OnInit, EventEmitter, Output } from '@angular/core';
   styleUrls: ['./search-bar.component.css']
 })
 export class SearchBarComponent implements OnInit {
-  @Output() submitted = new EventEmitter<string>();
   term ='';
   constructor() { }
 
