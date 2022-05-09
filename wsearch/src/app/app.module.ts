f3431105630a4e533a01ed50c538a44d31ae7675
Guillaume CLERIS <97674349+GuillaumeCleris@users.noreply.github.com>
2022-05-09 19:26:14 +0200

    obtenir la réponse d'une recherche sur l'api wikipedia

--- wsearch/src/app/app.module.ts
+++ wsearch/src/app/app.module.ts
@@ -5,6 +5,8 @@ import { AppComponent } from './app.component';
 import { SearchBarComponent } from './search-bar/search-bar.component';
 import { PageListComponent } from './page-list/page-list.component';
 
+import { HttpClientModule } from '@angular/common/http';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -12,7 +14,7 @@ import { PageListComponent } from './page-list/page-list.component';
     PageListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
