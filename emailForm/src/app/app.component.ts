5a20659d941a94a17585417d247ee9e05a3ef752
Guillaume CLERIS <97674349+GuillaumeCleris@users.noreply.github.com>
2022-05-13 11:59:44 +0200

    pré-template formulaire simple

--- emailForm/src/app/app.component.ts
+++ emailForm/src/app/app.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'emailForm';
+  email: string ='';
+  
+  /*constructor() {
+    setInterval(() => {
+    this.email += 'a';
+  }, 1000);
+  }*/
 }
