9c18695b079e11ac517b15ab51d1c8338fd21376
Guillaume CLERIS <97674349+GuillaumeCleris@users.noreply.github.com>
2022-05-14 22:14:58 +0200

    signup fonctionnement

--- emailclient/src/app/auth/auth.service.ts
+++ emailclient/src/app/auth/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-
+import { BehaviorSubject } from 'rxjs';
+import {tap } from 'rxjs/operators';
 interface UsernameAvailableResponse{
   available: boolean;
 }
@@ -19,6 +20,7 @@ interface SignupResponse {
 })
 export class AuthService {
   rootUrl = 'https://api.angular-email.com';
+  signedin$ = new BehaviorSubject(false);
   constructor(private http: HttpClient) { }
 
   usernameAvailable(username: string) {
@@ -29,9 +31,14 @@ export class AuthService {
   }
 
   signup(credentials: SignupCredentials){
-    return this.http.post<SignupResponse>(
+    return this.http
+    .post<SignupResponse>(
       this.rootUrl+'/auth/signup',
-      credentials
+      credentials)
+      .pipe(
+      tap(() => {
+        this.signedin$.next(true);
+      })
     );
   }
 
